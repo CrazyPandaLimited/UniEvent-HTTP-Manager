8cbb9000ce002ed57fc841eefe95d76024f10699
Ivan Baidakou <the.dmol@yandex.by>
2021-02-19 17:39:39 +0300

    [UniEvent-HTTP-Manager] prefork, spawn callback test

---
+++ plus/UniEvent-HTTP-Manager/t/lib/IPCToken.pm
@@ -0,0 +1,58 @@
+package IPCToken;
+use 5.016;
+use warnings;
+use IPC::SysV qw(IPC_CREAT IPC_PRIVATE);
+
+sub new {
+    my ($class, $initial) = @_;
+    my $id = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
+
+    my $obj = bless {id => $id} => $class;
+    $obj->inc($initial) if $initial;
+    return $obj;
+}
+
+sub inc {
+    my ($self, $val) = @_;
+    $val //= 1;
+    my $opstring = pack("s!s!s!", 0, $val, 0);
+    my $id = $self->{id};
+    semop($id, $opstring) || die "semop ($$): $!";
+}
+
+sub dec {
+    my ($self, $val) = @_;
+    ($val //= 1) *= -1;
+    my $opstring = pack("s!s!s!", 0, $val, 0);
+    my $id = $self->{id};
+    semop($id, $opstring) || die "semop ($$): $!";
+}
+
+sub await {
+    my ($self, $timeout) = @_;
+    eval {
+         local $SIG{ALRM} = sub { die "alarm\n" };
+         alarm $timeout;
+
+         my $opstring = pack("s!s!s!", 0, 0, 0);
+         my $id = $self->{id};
+         semop($id, $opstring) || die "semop ($$): $!";
+
+         alarm 0;
+     };
+     if ($@) {
+        if ($@ eq "alarm\n") {
+            return "timed out";
+        }
+        else {
+            return @$;
+        }
+     }
+     else {
+        return undef;
+     }
+}
+
+
+
+1;
